4736ac624568e1829cb18cf3e6a6370ab3c8a0d1
oganetti <37050193+oganetti@users.noreply.github.com>
2018-12-02 23:40:48 +0300

    First

---
+++ WebApplication1/Controllers/ListController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Controllers
+
+
+{
+
+    [Route("api/[controller]")]
+    public class ListController : Controller
+
+    {
+        private readonly IConfiguration configuration;
+
+
+
+
+        public ListController(IConfiguration config)
+        {
+            this.configuration = config;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+
+            ArrayList myList = new ArrayList();
+            string listName,typeList;
+            int? listID,parentID = null;
+            ListType lt;
+           
+            
+            string connectionstring = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand com = new SqlCommand("select * from list", connection);
+            var count = com.ExecuteReader();
+            while (count.Read())
+            {
+                listID = Convert.ToInt32(count["listID"]);
+                typeList = count["typeList"] as string;
+                listName = count["listName"] as string;
+                if(count["parentID"] != DBNull.Value)
+                    parentID = Convert.ToInt32(count["parentID"]);
+
+        
+                lt = new ListType(listID, listName, parentID, typeList);
+
+                myList.Add(lt);
+                
+
+
+            }
+            connection.Close();
+
+            return Json( myList );
+        }
+
+        public IActionResult Welcome(string name,int totalTimes = 5)
+        {
+            ViewData["message"] = "Hi" + name;
+            ViewData["totalTimes"] = totalTimes;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ListType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class ListType
+    {
+        public int? listID { get; set; }
+        public string listName { get; set; }
+        public int? parentID { get; set; }
+        public string typeList { get; set; }
+
+        public ListType(int? ID, string name,int? parent,string type)
+        {
+            listID = ID;
+            listName = name;
+            parentID = parent;
+            typeList = type;
+        }
+
+    }
+}
